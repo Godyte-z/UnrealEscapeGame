73331125e07800053fc4cf39f8f0736f4ff9330f
Goda Zalalyte <Goda@dhcp-10-248-138-174.eduroam.wireless.private.cam.ac.uk>
2017-10-24 09:51:53 +0100

    FindComponentByClass() had to make sure that #includes were in the correct order otherwise code would not compile (weird!)

--- EscapeGame/Source/EscapeGame/Grabber.cpp
+++ EscapeGame/Source/EscapeGame/Grabber.cpp
@@ -3,42 +3,53 @@
 #include "Grabber.h"
 #include "DrawDebugHelpers.h"
 
-#define OUT //THIS DOES NOTHING 
+#define OUT //THIS DOES NOTHING
 
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
+    
+    // ...
 }
 
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
-	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Grabber ready for duty!"));
-	
+    Super::BeginPlay();
+    
+    UE_LOG(LogTemp, Warning, TEXT("Grabber ready for duty!"));
+    
+    /// Look for attached Physics Handle
+   PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+    if (PhysicsHandle)
+    {
+        // Physics handle is found
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName())
+    }
+    
 }
 
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
     FVector PlayerViewPointLocation;
     FRotator PlayerViewPointRotation;
-	/// Get player view point this tick
+    /// Get player view point this tick
     GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint( OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
     
     ///Log out to test
-   /* UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString()); */
+    /* UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString()); */
     
     FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach); //drawing out a 50cm long line
     
--- EscapeGame/Source/EscapeGame/Grabber.h
+++ EscapeGame/Source/EscapeGame/Grabber.h
@@ -4,30 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ESCAPEGAME_API UGrabber : public UActorComponent //UActor here inherits the features from UGrabber
 {
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UGrabber();
-
+    GENERATED_BODY()
+    
+public:
+    // Sets default values for this component's properties
+    UGrabber();
+    
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    
+public:
+    // Called every frame
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
     
 private:
     // How far ahead of the player we can reach in cm
     float Reach = 100.f;
-
-		
-	
+    
+    UPhysicsHandleComponent* PhysicsHandle = nullptr;
+    
+    
+    
 };
+
